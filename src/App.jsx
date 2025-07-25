68bc2c787299aec8d79b4371e6a8314d286e0d3c
Minhdung09 <dungdm.dev@golazy.io>
2025-07-25 11:39:48 +0700

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { CartProvider } from "./contexts/CartContext";
 import Navbar from "./components/Navbar";
@@ -27,6 +27,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [news, setNews] = useState([]);
   const [error, setError] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -99,7 +100,7 @@ function App() {
                 <Route path="/checkout" element={<Checkout />} />
                 <Route path="/checkout/success" element={<CheckoutSuccess />} />
               </Routes>
-              <Footer />
+              {location.pathname === "/" && <Footer />}
             </div>
         </CartProvider>
       </Router>
